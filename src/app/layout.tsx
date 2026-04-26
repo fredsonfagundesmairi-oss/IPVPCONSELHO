4c90daabef74c31da82baf14944d429514add2c4
fredsonfagundesmairi-oss <fredsonfagundesmairi@gmail.com>
2026-04-26 07:52:17 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <head>
+        {/* Esta linha abaixo é o "pintor" do site. Ela carrega todos os visuais, cores e organizações do Tailwind */}
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="bg-slate-50 text-slate-900 antialiased min-h-screen">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
