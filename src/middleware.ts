2ae7e7f96d3bf24a42f242ba4289a3a20c1dd90e
fredsonfagundesmairi-oss <fredsonfagundesmairi@gmail.com>
2026-04-26 06:55:03 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,14 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
   const authCookie = request.cookies.get('ipvp_auth');
-  if (request.nextUrl.pathname.startsWith('/secretaria') && !authCookie) {
+
+  // Se tentar entrar na secretaria sem o cookie, manda para o login
+  if (pathname.startsWith('/secretaria') && !authCookie) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
+
   return NextResponse.next();
 }
 
